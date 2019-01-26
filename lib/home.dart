3f88b2b0c34f5dc3b07bcefac5bde0da4e0ec0ca
Dggz <cristianbaciu95@gmail.com>
2019-01-26 20:03:07 +0200

    init2

---
+++ lib/home.dart
@@ -0,0 +1,73 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:exam/addreminder.dart';
+
+import 'listreminders.dart';
+import 'reminder.dart';
+
+String globalURL() {
+  return 'http://192.168.0.102:2224';
+//  return 'http://192.168.0.102:8000/reminders/';
+//  return 'http://10.182.5.51:8000/reminders/';
+//  return 'http://10.182.5.51:8000/reminders/';
+}
+
+Future<List<Reminder>> fetchPosts(http.Client client) async {
+  final response = await http.get(globalURL() + "/genres");
+  if (response.statusCode == 200) {
+    // If the call to the server was successful, parse the JSON
+    final parsed = json.decode(response.body);
+
+    return parsed['results'].map<Reminder>((json) => Reminder.fromJson(json)).toList();
+  }
+  else {
+    // If that call was not successful, throw an error.
+    throw Exception('Failed to load post');
+  }
+}
+
+
+class HomePage extends StatelessWidget {
+  final String title;
+
+  HomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: FutureBuilder<List<Reminder>>(
+        future: fetchPosts(http.Client()),
+        builder: (context, snapshot) {
+          if (snapshot.hasError)
+            print(snapshot.error);
+          return snapshot.hasData
+              ? ListViewReminders(reminders: snapshot.data)
+              : Center(child: CircularProgressIndicator());
+        },
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.orange,
+        onPressed: () async {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddReminderScreen()),
+          );
+
+        },
+      ),
+    );
+  }
+}
---
+++ lib/listreminders.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:exam/editreminder.dart';
+
+import 'reminder.dart';
+
+
+class ListViewReminders extends StatelessWidget {
+  final List<Reminder> reminders;
+
+  ListViewReminders({Key key, this.reminders}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView.builder(
+          itemCount: reminders.length,
+          padding: const EdgeInsets.all(15.0),
+          itemBuilder: (context, position) {
+
+            return Column(
+              children: <Widget>[
+                Divider(height: 5.0),
+                ListTile(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ReminderScreen(reminder: reminders[position])),
+                  );
+                },
+                title: Text(
+                  '${reminders[position].title}',
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    color: Colors.deepOrangeAccent,
+                  ),
+                ),
+                subtitle: Text(
+                  '${reminders[position].text}',
+                  style: new TextStyle(
+                    fontSize: 18.0,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                ),
+              ],
+            );
+          }),
+    );
+  }
+}
